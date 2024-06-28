ed7710f9a8b96b01babf13f9553a77d2a83a0890
UmaAshaMS <umaasha69@gmail.com>
2024-06-28 11:28:36 +0530

    LeetCode-1

--- Two-Sum.js
+++ Two-Sum.js
@@ -8,7 +8,20 @@ Input: nums = [2,7,11,15], target = 9
 Output: [0,1]
 Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 */
-
+const nums = [3,2,4]
+let target = 6;
 var twoSum = function(nums, target) {
-    
+    var resultArr = []
+    for(let i = 0; i < nums.length; i++){
+        for(let j = i+1; j < nums.length; j++){
+            sum = nums[i] + nums[j];
+            if(sum === target){
+                resultArr.push(i);
+                resultArr.push(j);
+                return resultArr;
+            }
+        }        
+    }
+    return resultArr;
 };
+console.log(twoSum(nums,target));
